736d0464758f044803626e9884bec188705738a2
kaus-0987 <kaustav1966@gmail.com>
2025-11-19 16:06:21 +0530

    Added the google site verification

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,16 +7,17 @@ export const metadata = {
   title: "OEC India - Best Overseas Education Consultants for Study Abroad",
   description:
     "OEC India helps students achieve their dream of studying abroad. Get expert guidance on university selection, visas, scholarships & more for the UK, USA, Canada, Australia & Europe.",
-  other: {
-    "google-site-verification": "BBEJORnmYafy3WXKp8YM_vAEOoWDnJp7kh2XtH4-6LI",
-  },
   icons: {
     icon: [
       { url: "/oec.png" },
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
-    apple: { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    apple: {
+      url: "/apple-touch-icon.png",
+      sizes: "180x180",
+      type: "image/png",
+    },
     shortcut: "/oec.png",
   },
   openGraph: {
@@ -55,33 +56,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <head>
-        {/* Meta Pixel Code */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '864579596089023');
-fbq('track', 'PageView');
-            `,
-          }}
+        <meta
+          name="google-site-verification"
+          content="BBEJORnmYafy3WXKp8YM_vAEOoWDnJp7kh2XtH4-6LI"
         />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=864579596089023&ev=PageView&noscript=1"
-            alt=""
-          />
-        </noscript>
-        {/* End Meta Pixel Code */}
       </head>
       <body>
         <Header />
